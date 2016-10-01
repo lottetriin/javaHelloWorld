dbf5994da93a60bba4de7205400ba7b539c18577
lottetriin <lottetriinn@gmail.com>
2016-10-01 14:37:44 +0300

    Update Main.java

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String args[]) {
         System.out.println("Hello World!");
         System.out.println("Autor: Lotte");
+        System.out.println("Aasta 2016");
     }
 }
